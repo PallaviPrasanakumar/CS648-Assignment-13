16a3851f207e83dd3a841a19b1c15be7aec526ce
Pallavi <68636048+PallaviPrasanakumar@users.noreply.github.com>
2022-12-14 15:55:30 -0800

    Assignment

---
+++ build/precache-manifest.3d9afd8e0aa2368411eb6039ba343ccb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd28c1c1b524f3564e80bcf8a5974b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "705846d66829c85a6ac7",
+    "url": "/static/css/main.324c2a1d.chunk.css"
+  },
+  {
+    "revision": "23abcbfefca95e9ed58c",
+    "url": "/static/js/2.7beeb27e.chunk.js"
+  },
+  {
+    "revision": "705846d66829c85a6ac7",
+    "url": "/static/js/main.95b7c19c.chunk.js"
+  },
+  {
+    "revision": "d43e78c8bbb21d4f0e7e",
+    "url": "/static/js/runtime-main.c35eacba.js"
+  }
+]);
